c9d3dd3e4cbcf4db096b48d7e2a0705445d4dccc
StormCloud <RabidRenamon@gmail.com>
2017-03-11 23:21:41 -0700

    Added Mod Class

---
+++ src/main/java/StormCloud/madengineering/MadEngineering.java
@@ -0,0 +1,15 @@
+package StormCloud.madengineering;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = MadEngineering.MODID, name = MadEngineering.MODNAME, version = MadEngineering.MODVERSION)
+public class MadEngineering {
+
+	public static final String MODID = "madengineering";
+	public static final String MODNAME = "Mad Engineering";
+	public static final String MODVERSION = "0.1.0";
+	
+	
+
+
+}
