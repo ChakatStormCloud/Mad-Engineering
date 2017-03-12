95c6e86577d6915c599ed0684ba488cc8e107d05
StormCloud <RabidRenamon@gmail.com>
2017-03-12 10:39:57 -0600

    End of day

--- src/main/java/StormCloud/madengineering/MadEngineering.java
+++ src/main/java/StormCloud/madengineering/MadEngineering.java
@@ -4,12 +4,12 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod(modid = MadEngineering.MODID, name = MadEngineering.MODNAME, version = MadEngineering.MODVERSION)
 public class MadEngineering {
-
+	
 	public static final String MODID = "madengineering";
 	public static final String MODNAME = "Mad Engineering";
 	public static final String MODVERSION = "0.1.0";
 	
+	@Mod.Instance(MODID)
+	public static MadEngineering instance;
 	
-
-
 }
